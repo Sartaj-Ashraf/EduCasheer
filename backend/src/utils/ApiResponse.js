313c549093917dbba3f731b5491ec2641460b920
Sartaj Ashraf <89686710+Sartaj-Ashraf@users.noreply.github.com>
2025-02-02 04:44:46 +0000

    Register user controller

--- backend/src/utils/ApiResponse.js
+++ backend/src/utils/ApiResponse.js
@@ -1,8 +1,9 @@
 class ApiResponse {
-    constructor(statusCode, message="Success", data, success) {
+    constructor(statusCode, message = "Success", data, success) {
         this.statusCode = statusCode
         this.data = data
         this.message = message
         this.success = success < 400
     }
 }
+export { ApiResponse }
