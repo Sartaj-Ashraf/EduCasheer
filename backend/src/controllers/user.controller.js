9816da0ae771c6db964f0e9e579c1e88cc2b8ecd
Sartaj Ashraf <89686710+Sartaj-Ashraf@users.noreply.github.com>
2025-02-03 06:12:37 +0000

    Auth controllers

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -3,6 +3,25 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
+
+// Generate tokens
+const generateAccessAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating tokens");
+
+    }
+
+}
 // Validate user inputs 
 const validateUserInput = (fullName, username, email, password) => {
     if ([fullName, username, email, password].some((field) => !field?.trim())) {
@@ -53,7 +72,7 @@ const registerUser = asyncHandler(async (req, res) => {
         // Check for existing user
         const existingUser = await User.findOne({
             $or: [
-                { username: username.toLowerCase() },
+                // { username: username.toLowerCase() },
                 { email: email.toLowerCase() }
             ]
         });
@@ -96,4 +115,61 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 });
 
-export { registerUser };
+
+
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password, username } = req.body;
+    if (!email && !password) {
+        throw new ApiError(400, "Email and password are required");
+    }
+    const user = await User.findOne({
+        $or: [
+            { email: email.toLowerCase() },
+            // { username: username.toLowerCase() }
+        ]
+    });
+    if (!user) {
+        throw new ApiError(404, "User not found");
+    }
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid password");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id);
+    const loggedInUser = await User.findById(user._id)
+        .select("-password -refreshToken");
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+    return res.status(200)
+        .cookie('accessToken', accessToken, options)
+        .cookie('refreshToken', refreshToken, options)
+        .json(new ApiResponse(200, {
+            user: loggedInUser,
+            accessToken,
+            refreshToken,
+            message: "Logged in successfully"
+        }
+        ));
+})
+
+
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(req?.user?._id,
+        {
+            $set:
+                { refreshToken: undefined }
+        },
+        { new: true });
+
+    return res.status(200)
+        .clearCookie('accessToken', options)
+        .clearCookie('refreshToken', options)
+        .json(new ApiResponse(200, "Logged out successfully"))
+        .json(new ApiResponse(200, "Logged out successfully"));
+})
+
+export { registerUser, loginUser, logoutUser };
